fb57ddf1b26f9803bab775b57a6d256c115562a5
Carlos Andres Aguinaga Núñez <andres.aguinaga@gmail.com>
2017-06-19 12:00:59 -0500

    My model - Add get_result()

--- MY_Model.php
+++ MY_Model.php
@@ -4,9 +4,32 @@ class MY_Model extends CI_Model {
 
     private $_table = '';
 
+    private $_query_fields = '*';
+
     function __construct(){
         parent::__construct();
+    }
 
-        
+    private function get_result () {
+
+		if($this->select_only_actives){
+			$this->db->where($this->_table . '.status', 'status_enabled');
+		}
+
+		$query_obj = $this->db->select($this->_query_fields)
+							->from($this->_table)
+							->where($condition, null, false);
+
+        if (substr($this->order_by, 0, 1) === "-") {
+
+            $query_obj->order_by(substr($this->order_by, 1), "DESC");
+        } else {
+
+            $query_obj->order_by($this->order_by, "DESC");
+        }
+
+        $query = $query_obj->get();
+
+        return $query->result();
     }
 }
