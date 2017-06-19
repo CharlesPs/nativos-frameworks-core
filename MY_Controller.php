f7583c4a88d01fbca8b2bf9a41a9f0384010a1aa
Carlos Andres Aguinaga Núñez <andres.aguinaga@gmail.com>
2017-06-19 17:01:42 -0500

    My Controller, movemos el m_control

--- MY_Controller.php
+++ MY_Controller.php
@@ -2,12 +2,21 @@
 
 class MY_Controller extends CI_Controller {
 
-    function __construct(){
+    function __construct () {
 		parent::__construct();
-
-        $this->load->model('m_control');
 
 		$this->load->helper("url");
 		$this->load->helper("date");
+
+        $this->salida = array();
 	}
+}
+
+class Client_Controller extends MY_Controller {
+
+    function __construct () {
+        parent::__construct();
+
+        $this->load->model('m_control');
+    }
 }
